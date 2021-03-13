c8ab9acd0443b4565ff3e2a4e273ebf43d5d99a7
lP0f1g1stl <64088803+lP0f1g1stl@users.noreply.github.com>
2021-03-13 17:29:39 +0300

    !!!!

---
+++ Clicker Test/Assets/Scripts/GameButtons.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameButtons : MonoBehaviour
+{
+    [SerializeField] private GameObject background;
+    [SerializeField] private GameObject mineralSpawner;
+    [SerializeField] private GameObject upgradesList;
+    [SerializeField] private GameObject acceptExit;
+
+    private float speed = 0.1f;
+
+    public void StartOnClick()
+    {
+        background.GetComponent<BackgroundScrolling>().SetBackgroundSpeed(speed);
+        mineralSpawner.GetComponent<MineralSpawner>().StartStop(speed);
+    }
+
+    public void ShowUpgrades() 
+    {
+        upgradesList.SetActive(!upgradesList.activeSelf);
+    }
+
+    public void Exit(int commId) 
+    {
+        if (commId == 0)
+        {
+            acceptExit.SetActive(!acceptExit.activeSelf);
+        }
+        if (commId == 1)
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Clicker Test/Assets/Scripts/Upgrades.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Upgrades : MonoBehaviour
+{
+    [SerializeField] private int[,] valuesOfUpgrades = new int[3, 3];
+
+    [SerializeField] private Text[] descOfLevels = new Text[3];
+    [SerializeField] private Text[] descOfPrices = new Text[3];
+
+    private int money;
+
+    void Start() 
+    {
+        valuesOfUpgrades[0, 0] = 1;
+        valuesOfUpgrades[1, 0] = 1;
+        valuesOfUpgrades[2, 0] = 1;
+
+        valuesOfUpgrades[0, 1] = 1;
+        valuesOfUpgrades[1, 1] = 1;
+        valuesOfUpgrades[2, 1] = 1;
+
+        valuesOfUpgrades[0, 2] = 10;
+        valuesOfUpgrades[1, 2] = 10;
+        valuesOfUpgrades[2, 2] = 3;
+    }
+
+    public void SetUpgradeLevel(int upgradeId) 
+    {
+        if (money >= valuesOfUpgrades[upgradeId, 1] && valuesOfUpgrades[upgradeId, 0] < valuesOfUpgrades[upgradeId, 2]) 
+        {
+            GetComponent<Score>().ChangeAmountOfMoney(-valuesOfUpgrades[upgradeId, 1]);
+            valuesOfUpgrades[upgradeId, 0] += 1;
+            valuesOfUpgrades[upgradeId, 1] *= 2;
+            descOfLevels[upgradeId].text = valuesOfUpgrades[upgradeId, 0].ToString();
+            descOfPrices[upgradeId].text = valuesOfUpgrades[upgradeId, 1].ToString();
+            GetUpgradeValue(upgradeId);
+        }
+    }
+
+    public void CheckScore(int cMoney) 
+    {
+        money = cMoney;
+    }
+
+    private void GetUpgradeValue( int upgradeId) 
+    {
+        if (upgradeId == 0) 
+        {
+            GetComponent<MineralSpawner>().SetSpawnSpeed();
+        }
+        if (upgradeId == 1) 
+        {
+            GetComponent<MineralSpawner>().SetPriceOfUpgrade(valuesOfUpgrades[upgradeId, 0]);
+        }
+        if (upgradeId == 2) 
+        {
+            GetComponent<MineralSpawner>().SetMineralSpawnTier(valuesOfUpgrades[upgradeId, 0]);
+        }
+    }
+}
